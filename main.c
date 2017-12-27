fcc5e9a44bcef0ac1dc1b2c03f6d04ec000ad82e
App <shobhitsagar109@gmail.com>
2017-12-27 12:55:59 +0530

    Made some changes

--- main.c
+++ main.c
@@ -9,24 +9,26 @@
 #include <stdio.h>
 
 void towerFun(int, char, char, char);
+
 int main()
 {
-    int n;
+    int number;
     printf("Enter the number of disks \n");
-    scanf("%d",&n);
-    printf("The sequence on moves involved in %d number of disks in Tower of Hanoi are : \n",n);
-    towerFun(n, 'A', 'C', 'B');
+    scanf("%d",&number);
+    printf("The sequence on moves involved in %d number of disks in Tower of Hanoi are : \n",number);
+    towerFun(number, 'A', 'C', 'B');
     return(0);
 }
 
-void towerFun(int n, char fr, char tr, char ar)
+void towerFun(int num, char fr, char tr, char ar)
 {
-    if(n==1)
+    if(num==1)
     {
         printf("Move disk 1 from %c to %c \n",fr,tr);
         return;
     }
-    towerFun(n-1, fr, ar, tr);
-    printf("Move disk %d from %c to %c \n",n,fr,tr);
-    towerFun(n-1, ar, tr, fr);
+    towerFun(num-1, fr, ar, tr);
+    printf("Move disk %d from %c to %c \n",num,fr,tr);
+    towerFun(num-1, ar, tr, fr);
 }
+
